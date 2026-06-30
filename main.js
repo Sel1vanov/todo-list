711c8e1e07fbfe842fb01e6b187f9c19ff9e60aa
Denis <82977493+Sel1vanov@users.noreply.github.com>
2026-06-30 16:44:51 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -11,6 +11,7 @@ function resetInput(event){
     descriptionTask.value = ""
 }
 resetButton.addEventListener('click', resetInput)
+
 //подвязка кнопки с событием
 let tasks;
 if(localStorage.getItem('tasks') !== null) tasks = JSON.parse(localStorage.getItem('tasks'))
@@ -21,7 +22,8 @@ function newTask(){
     if(!title || !description) return(alert("Введите название и описание."))
     tasks.push({
         name: title,
-        desc: description
+        desc: description,
+        status: false
     })
     console.log(tasks)
     render()
@@ -32,19 +34,24 @@ function render(){
     tasksList.innerHTML = ''
     tasks.forEach(function(task, index){
         const cardHTML = `
-        <div class="card-task">
+        <div class="card-task ${task.status ? 'is-done' : ''}">
                     <h3>${task.name}</h3>
                     <p>${task.desc}</p>
+                    <span class="done-badge">Выполнено</span>
                         <div class="card-buttons">
-                        <md-filled-tonal-icon-button toggle>
-                        <md-icon>done</md-icon>
-                        <md-icon slot="selected">close</md-icon>
-                        </md-filled-tonal-icon-button>
-                        <md-filled-icon-button class="deleteBTN" data-index="${index}">
-                        <md-icon>delete</md-icon>
-                        </md-filled-icon-button>
-                        <div>
-                </div>
+                            <md-filled-tonal-icon-button 
+                            toggle
+                            class="done-button"
+                            data-index="${index}"
+                            ${task.status ? 'selected' : ""}>
+                                <md-icon>done</md-icon>
+                            <md-icon slot="selected">close</md-icon>
+                            </md-filled-tonal-icon-button>
+                            <md-filled-icon-button class="deleteBTN" data-index="${index}">
+                                <md-icon>delete</md-icon>
+                            </md-filled-icon-button>
+                        </div>
+        </div>
         `
         tasksList.insertAdjacentHTML('beforeend', cardHTML)
     })
@@ -64,7 +71,21 @@ tasksList.addEventListener('click', function(event){
     if(deleteButton){
         const index = deleteButton.dataset.index;
         deleteTask(index)
+        return
     }
+
+    const doneButton = event.target.closest('.done-button')
+
+    if(doneButton){
+        const index = doneButton.dataset.index;
+        tasks[index].status = !tasks[index].status
+
+        localStorage.setItem('tasks', JSON.stringify(tasks))
+        render()
+
+    }
+    
 })
+
 
 render()
